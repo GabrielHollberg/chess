70fc38599d200f7f43995f352b61217f5904ee4d
Gabriel Hollberg <gthollberg@gmail.com>
2024-01-18 12:14:00 -0700

    Added data structure (pieces) to ChessBoard.java and implemented addPiece() and getPiece()

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -18,8 +18,9 @@ public class ChessBoard {
      * @param position where to add the piece to
      * @param piece    the piece to add
      */
-    public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+    public void addPiece(ChessPosition position, ChessPiece piece)
+    {
+        pieces[position.getRow()][position.getColumn()] = piece;
     }
 
     /**
@@ -29,15 +30,19 @@ public class ChessBoard {
      * @return Either the piece at the position, or null if no piece is at that
      * position
      */
-    public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+    public ChessPiece getPiece(ChessPosition position)
+    {
+        return pieces[position.getRow()][position.getColumn()];
     }
 
     /**
      * Sets the board to the default starting board
      * (How the game of chess normally starts)
      */
-    public void resetBoard() {
+    public void resetBoard()
+    {
         throw new RuntimeException("Not implemented");
     }
+
+    private ChessPiece[][] pieces = new ChessPiece[8][8];
 }
