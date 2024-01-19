3181aee72c1732f5bb0ecfaa063d93798576dabd
Gabriel Hollberg <gthollberg@gmail.com>
2024-01-19 12:43:25 -0700

    Implemented getters and variables

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -10,7 +10,10 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
-    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type)
+    {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -28,15 +31,17 @@ public class ChessPiece {
     /**
      * @return Which team this chess piece belongs to
      */
-    public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+    public ChessGame.TeamColor getTeamColor()
+    {
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
-    public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+    public PieceType getPieceType()
+    {
+        return type;
     }
 
     /**
@@ -49,4 +54,7 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         throw new RuntimeException("Not implemented");
     }
+
+    private ChessGame.TeamColor pieceColor;
+    private ChessPiece.PieceType type;
 }
