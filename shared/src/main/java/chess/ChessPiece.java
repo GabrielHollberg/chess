c66adba08db2cc3799b5ce863df6f730d02e3022
Gabriel Hollberg <gthollberg@gmail.com>
2024-09-20 19:23:45 -0700

    Everything except pawnMoves passes tests.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -76,55 +76,276 @@ public class ChessPiece {
             return null;
         } else if(board.getPiece(myPosition).type == PieceType.ROOK) {
             return rookMoves(board, myPosition);
-        } else {
+        } else if(board.getPiece(myPosition).type == PieceType.KNIGHT) {
+            return knightMoves(board, myPosition);
+        } else if(board.getPiece(myPosition).type == PieceType.BISHOP) {
+            return bishopMoves(board, myPosition);
+        } else if(board.getPiece(myPosition).type == PieceType.QUEEN) {
+            return queenMoves(board, myPosition);
+        } else if(board.getPiece(myPosition).type == PieceType.KING) {
+            return kingMoves(board, myPosition);
+        } /*else if(board.getPiece(myPosition).type == PieceType.PAWN) {
+            return pawnMoves(board, myPosition);
+        }*/ else {
             return null;
         }
     }
 
     public Collection<ChessMove> rookMoves(ChessBoard board, ChessPosition myPosition) {
-        ArrayList<ChessMove> moves = new ArrayList<ChessMove>();
-        xIt = 1;
-        yIt = 0;
-        xTracker = myPosition.getRow();
-        yTracker = myPosition.getColumn();
+        this.board = board;
         this.myPosition = myPosition;
+        ArrayList<ChessMove> moves = new ArrayList<ChessMove>();
 
         // Iterate through four movement directions
         for(int i = 0; i < 4; i++) {
-            do {
-                ChessPosition newPosition = new ChessPosition(xTracker + xIt + 1, yTracker + yIt + 1);
-                if(newPosition.getRow() <= 7 && newPosition.getRow() >= 0 && newPosition.getColumn() <= 7 && newPosition.getColumn() >= 0 && (board.getPiece(newPosition) == null || board.getPiece(newPosition).getTeamColor() != board.getPiece(myPosition).getTeamColor())) {
-                    moves.add(new ChessMove(myPosition, newPosition));
-                    if(board.getPiece(newPosition) != null && board.getPiece(newPosition).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                        break;
-                    }
-                } else {
-                    break;
-                }
-                xTracker += xIt;
-                yTracker += yIt;
-            } while (xTracker + xIt >= 0 && xTracker + xIt <= 7 && yTracker + yIt >= 0 && yTracker + yIt <= 7);
-
-            // Update iterators and reset row and column trackers
-            updateIt(i);
+            // Reset row and column trackers
             resetTrackers();
+            // Update Iterators
+            updateIt(i);
+            // Iterate through squares
+            checkSquares(moves);
         }
         return moves;
     }
 
-    public void updateIt(int i) {
-        switch(i) {
-            case 0:
+    public Collection<ChessMove> knightMoves(ChessBoard board, ChessPosition myPosition) {
+        this.board = board;
+        this.myPosition = myPosition;
+        ArrayList<ChessMove> moves = new ArrayList<ChessMove>();
+
+        // Iterate through four movement directions
+        for(int i = 0; i < 8; i++) {
+            // Reset row and column trackers
+            resetTrackers();
+            // Update Iterators
+            updateIt(i);
+            // Iterate through squares
+            checkSquares(moves);
+        }
+        return moves;
+    }
+
+    public Collection<ChessMove> bishopMoves(ChessBoard board, ChessPosition myPosition) {
+        this.board = board;
+        this.myPosition = myPosition;
+        ArrayList<ChessMove> moves = new ArrayList<ChessMove>();
+
+        // Iterate through four movement directions
+        for(int i = 0; i < 4; i++) {
+            // Reset row and column trackers
+            resetTrackers();
+            // Update Iterators
+            updateIt(i);
+            // Iterate through squares
+            checkSquares(moves);
+        }
+        return moves;
+    }
+
+    public Collection<ChessMove> queenMoves(ChessBoard board, ChessPosition myPosition) {
+        this.board = board;
+        this.myPosition = myPosition;
+        ArrayList<ChessMove> moves = new ArrayList<ChessMove>();
+
+        // Iterate through eight movement directions
+        for(int i = 0; i < 8; i++) {
+            // Reset row and column trackers
+            resetTrackers();
+            // Update Iterators
+            updateIt(i);
+            // Iterate through squares
+            checkSquares(moves);
+        }
+        return moves;
+    }
+
+    public Collection<ChessMove> kingMoves(ChessBoard board, ChessPosition myPosition) {
+        this.board = board;
+        this.myPosition = myPosition;
+        ArrayList<ChessMove> moves = new ArrayList<ChessMove>();
+
+        // Iterate through eight movement directions
+        for(int i = 0; i < 8; i++) {
+            // Reset row and column trackers
+            resetTrackers();
+            // Update Iterators
+            updateIt(i);
+            // Iterate through squares
+            checkSquares(moves);
+        }
+        return moves;
+    }
+
+    /*public Collection<ChessMove> pawnMoves(ChessBoard board, ChessPosition myPosition) {
+        this.board = board;
+        this.myPosition = myPosition;
+        ArrayList<ChessMove> moves = new ArrayList<ChessMove>();
+
+        if(board.getPiece(myPosition).getTeamColor() == ChessGame.TeamColor.WHITE) {
+
+            if(myPosition.getRow() == 1) {
+                resetTrackers();
                 xIt = 0;
                 yIt = 1;
+                do {
+                    xTracker += xIt;
+                    yTracker += yIt;
+                    if()
+                }
+            } else {
+
+            }
+
+        }
+
+        // Iterate through eight movement directions
+        for(int i = 0; i < 8; i++) {
+            // Reset row and column trackers
+            resetTrackers();
+            // Update Iterators
+            updateIt(i);
+            // Iterate through squares
+            checkSquares(moves);
+        }
+        return moves;
+    }*/
+
+    public void checkSquares(ArrayList<ChessMove> moves) {
+        switch(board.getPiece(myPosition).getPieceType()) {
+            case ROOK, QUEEN, BISHOP:
+                do {
+                    ChessPosition newPosition = new ChessPosition(xTracker + xIt + 1, yTracker + yIt + 1);
+                    if (isPositionInBounds(newPosition)) {
+                        if (board.getPiece(newPosition) == null) {
+                            moves.add(new ChessMove(myPosition, newPosition));
+                        } else if (isPositionEnemy(newPosition)) {
+                            moves.add(new ChessMove(myPosition, newPosition));
+                            break;
+                        } else {
+                            break;
+                        }
+                    }
+                    xTracker += xIt;
+                    yTracker += yIt;
+                } while (xTracker + xIt >= 0 && xTracker + xIt <= 7 && yTracker + yIt >= 0 && yTracker + yIt <= 7);
                 break;
-            case 1:
-                xIt = -1;
-                yIt = 0;
+            case KNIGHT, KING:
+                ChessPosition newPosition = new ChessPosition(xTracker + xIt + 1, yTracker + yIt + 1);
+                if (isPositionInBounds(newPosition)) {
+                    if (board.getPiece(newPosition) == null) {
+                        moves.add(new ChessMove(myPosition, newPosition));
+                    } else if (isPositionEnemy(newPosition)) {
+                        moves.add(new ChessMove(myPosition, newPosition));
+                        break;
+                    } else {
+                        break;
+                    }
+                }
                 break;
-            case 2:
-                xIt = 0;
-                yIt = -1;
+        }
+    }
+
+    public boolean isPositionInBounds(ChessPosition newPosition) {
+        return (newPosition.getRow() <= 7 && newPosition.getRow() >= 0 && newPosition.getColumn() <= 7 && newPosition.getColumn() >= 0);
+    }
+
+    public boolean isPositionEnemy(ChessPosition newPosition) {
+        return (board.getPiece(newPosition).getTeamColor() != board.getPiece(myPosition).getTeamColor());
+    }
+
+    public void updateIt(int i) {
+        switch(board.getPiece(myPosition).getPieceType()) {
+            case ROOK:
+                switch (i) {
+                    case 0:
+                        xIt = 1;
+                        yIt = 0;
+                        break;
+                    case 1:
+                        xIt = 0;
+                        yIt = 1;
+                        break;
+                    case 2:
+                        xIt = -1;
+                        yIt = 0;
+                        break;
+                    case 3:
+                        xIt = 0;
+                        yIt = -1;
+                        break;
+                }
+                break;
+            case KNIGHT:
+                switch (i) {
+                    case 0:
+                        xIt = 2;
+                        yIt = 1;
+                        break;
+                    case 1:
+                        xIt = 2;
+                        yIt = -1;
+                        break;
+                    case 2:
+                        xIt = -2;
+                        yIt = 1;
+                        break;
+                    case 3:
+                        xIt = -2;
+                        yIt = -1;
+                        break;
+                    case 4:
+                        xIt = 1;
+                        yIt = 2;
+                        break;
+                    case 5:
+                        xIt = 1;
+                        yIt = -2;
+                        break;
+                    case 6:
+                        xIt = -1;
+                        yIt = 2;
+                        break;
+                    case 7:
+                        xIt = -1;
+                        yIt = -2;
+                        break;
+                }
+                break;
+            case BISHOP, QUEEN, KING:
+                switch (i) {
+                    case 0:
+                        xIt = 1;
+                        yIt = 1;
+                        break;
+                    case 1:
+                        xIt = -1;
+                        yIt = 1;
+                        break;
+                    case 2:
+                        xIt = 1;
+                        yIt = -1;
+                        break;
+                    case 3:
+                        xIt = -1;
+                        yIt = -1;
+                        break;
+                    case 4:
+                        xIt = 1;
+                        yIt = 0;
+                        break;
+                    case 5:
+                        xIt = 0;
+                        yIt = 1;
+                        break;
+                    case 6:
+                        xIt = -1;
+                        yIt = 0;
+                        break;
+                    case 7:
+                        xIt = 0;
+                        yIt = -1;
+                        break;
+                }
                 break;
         }
     }
@@ -138,6 +359,7 @@ public class ChessPiece {
     private int yIt;
     private int xTracker;
     private int yTracker;
+    private ChessBoard board;
     private ChessPosition myPosition;
     private final ChessGame.TeamColor color;
     private final PieceType type;
