4b35faa8ab27e0add6b1dd9c851b2f1568e75043
Gabriel Hollberg <gthollberg@gmail.com>
2026-03-04 19:57:31 -0700

    Implemented createUser and readUser methods in UserService and createAuth in AuthService

--- server/src/main/java/service/AuthService.java
+++ server/src/main/java/service/AuthService.java
@@ -1,4 +1,15 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import model.AuthData;
+
 public class AuthService {
+
+    private AuthDAO authDAO;
+
+    public void createAuth(String authToken, String username) throws DataAccessException {
+        AuthData auth = new AuthData(authToken, username);
+        authDAO.createAuth(auth);
+    }
 }
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,19 @@
 package service;
 
+import dataaccess.UserDAO;
+import dataaccess.DataAccessException;
+import model.UserData;
+
 public class UserService {
+
+    private UserDAO userDAO;
+
+    public void createUser(String username, String password, String email) throws DataAccessException {
+        UserData user = new UserData(username, password, email);
+        userDAO.createUser(user);
+    }
+
+    public UserData readUser(String username) throws DataAccessException {
+        return userDAO.readUser(username);
+    }
 }
