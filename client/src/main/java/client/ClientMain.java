934211fcb57e7487c032b6577eb1b55a6abe12e1
Gabriel Hollberg <gthollberg@gmail.com>
2026-04-02 12:27:31 -0600

    Started working on REPL

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -2,9 +2,19 @@ package client;
 
 import chess.*;
 
+import java.util.Scanner;
+
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        System.out.println("type help to see options\n");
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("----------Chess----------\n");
+            String line = scanner.nextLine();
+            System.out.println();
+            if (line.equalsIgnoreCase("help") || line.equalsIgnoreCase("h")) {
+                System.out.println("Options\n");
+            }
+        }
     }
 }
